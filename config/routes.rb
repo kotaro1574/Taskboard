bc5541b884b4f6928851f7e4308bf0604129a529
kotaro1574 <shake1574@gmail.com>
2020-11-23 11:15:35 +0900

    ユーザ情報の編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' } 
+
   root 'top#index'
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
 
   resources :list, only: %i(new create edit update destroy) do
     resources :card, except: %i(index)
